295e90cd7a98c5dca3c72e2dfba45ab66d3eea6b
deepanshu bisht <bishtdeepanshu007@gmail.com>
2022-08-18 12:26:13 +0530

    Init commit

---
+++ src/app.js
@@ -0,0 +1,118 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+const app = express()
+const publicDirectoryPath=path.join(__dirname,'../public')
+const viewPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname , '../templates/partials')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+app.set('view engine','hbs')
+app.set('views',viewPath)
+hbs.registerPartials(partialsPath)
+//setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+//app.com
+app.get('',(req,res)=>{
+  res.render('index',{
+      title : 'Weather',
+      name : 'Deepanshu'
+  })
+})
+
+//app.com/help
+app.get('/help',(req,res)=>{
+  res.render('help',{
+    message : 'Do you need any kind of help !!',
+    title : 'Help',
+    name : 'Deepanshu'
+  })
+})
+// app.use(express.static())
+// app.get('/help',(req,res)=>{
+//   res.send({
+//      name : 'Deepanshu',
+//      age : 20
+//   })
+// })
+
+// res.send([{
+//     name : 'Deepansh'
+// },{
+//     name : 'ketan'
+// },{
+//     name : 'jatin'
+// }])
+// })
+
+//app.com/about
+app.get('/about',(req,res)=>{
+  res.render('about',{
+    title : 'About',
+    name : 'Deepanshu'
+  })
+})
+//app.com/weather
+app.get('/weather',(req,res)=>{
+  if(!req.query.address){
+     return res.send({
+      error : 'You need to provide an address'
+     })
+  }
+  geocode(req.query.address,(error , {latitude,longitude,location} = {})=>{
+     if(error){
+      return res.send({error})
+     }
+     
+     forecast(longitude,latitude,(error , forecastData )=>{
+        if(error){
+          return res.send({error})
+        }
+        res.send({
+          forecast : forecastData ,
+          location ,
+          address : req.query.address
+        })
+     })
+  })
+ 
+//   res.send({
+//      forecast : mainForecast,
+//      location : details,
+//      address :   req.query.address
+//   })
+})
+
+app.get('/products', (req,res)=>{
+    if(!req.query.search){
+      return res.send({
+        error : 'You must provide a search term'
+      })
+    }
+    console.log(req.query.search)
+    res.send({
+      product : []
+    })
+})
+
+//routes :- basically they all are the express routes
+app.get('/help/*' , (req,res)=>{
+   res.render('error',{
+     name : 'Deepanshu' ,
+     title : '404',
+     message : 'Help article not found'
+   })
+})
+
+app.get('*' , (req,res)=>{
+   res.render('error',{
+    name : 'Deepanshu',
+    title : '404',
+    message : 'Page not found !'
+   })
+})
+
+app.listen(3000,()=>{
+    console.log('Server is up on port 3000')
+})
