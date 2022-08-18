ac8723277053a9ee5e051ab64a2d7548da856241
deepanshu bisht <bishtdeepanshu007@gmail.com>
2022-08-18 13:03:49 +0530

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,4 @@
-console.log('Client side server !')
 // for the html doc which we require for showing up the result 
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const message1 = document.querySelector('#message-1')
@@ -8,7 +6,7 @@ const message2 = document.querySelector('#message-2')
 weatherForm.addEventListener('submit',(e)=>{
      message1.textContent = 'Loading ...'
      message2.textContent = ''
-     e.preventDefault();
+     e.preventDefault()
      const location = search.value
      fetch('http://localhost:3000/weather?address='+location).then((response)=>{
      response.json().then((data)=>{
