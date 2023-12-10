cee9d9ef524dc281cdcf7fe83e5ae697c2ae43cf
Hitansh <hitansh.gupta@kristalball.com>
2023-12-10 22:43:45 +0530

    edited

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -4,7 +4,7 @@ const geocode = (address,callback)=>{
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiZGVlcGFuc2h1YmlzaHQ5OSIsImEiOiJjbDZxdHc0aGkwa2ZtM2Jvd2Z5czNpa3Z0In0.LmIzKuuMpYX58ni00LKTzg&limit=1'
     request({ url , json:true},(error,{body})=>{
       if(error) {
-         callback('Unable to connect to locaton services',undefined)
+         callback('Unable to connect to location services',undefined)
       }
       else if(body.features.length === 0) {
          callback('Unable to find the location',undefined)
